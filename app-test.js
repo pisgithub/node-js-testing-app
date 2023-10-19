5560d1a87f69054d4bc3674c1398415c5c10f089
pisgithub <sohail.awan.official@live.com>
2023-10-19 18:56:00 +0500

    update tests

--- app-test.js
+++ app-test.js
@@ -5,7 +5,7 @@ const add = require('./app');
 assert.strictEqual(add(2, 3), 5, 'Addition of 2 + 3 should equal 5');
 
 // Test case 2: Addition of zero and a positive number
-assert.strictEqual(add(0, 7), 7, 'Addition of 0 + 7 should equal 7');
+assert.strictEqual(add(5, 7), 7, 'Addition of 0 + 7 should equal 7');
 
 // Test case 3: Addition of negative numbers
 assert.strictEqual(add(-4, -2), -6, 'Addition of -4 + -2 should equal -6');
